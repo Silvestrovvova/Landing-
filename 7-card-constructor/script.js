7e611b581c33bc3d1f30608965a2a8d805e51468
Silvestrovvova <sidni168@gmail.com>
2026-01-26 17:50:05 +0300

    added grid layout

--- 7-card-constructor/script.js
+++ 7-card-constructor/script.js
@@ -1,19 +1,45 @@
-const observerOptions = {
-  threshold: 0.1,
-};
+let currentIndex = 0;
+const images = document.querySelectorAll(".gallery-item img");
 
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.style.opacity = "1";
-      entry.target.style.transform = "translateY(0)";
-    }
-  });
-}, observerOptions);
+function openModal(element) {
+  const modal = document.getElementById("myModal");
+  const modalImg = document.getElementById("imgFull");
 
-document.querySelectorAll(".card").forEach((card) => {
-  card.style.opacity = "0";
-  card.style.transform = "translateY(40px)";
-  card.style.transition = "all 0.8s cubic-bezier(0.25, 0.46, 0.45, 0.94)";
-  observer.observe(card);
+  // Находим индекс нажатой картинки
+  const clickedImg = element.querySelector("img");
+  currentIndex = Array.from(images).indexOf(clickedImg);
+
+  modal.style.display = "flex";
+  modalImg.src = clickedImg.src;
+}
+
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
+
+// Функция для переключения фото
+function changeImage(step) {
+  currentIndex += step;
+  if (currentIndex >= images.length) currentIndex = 0;
+  if (currentIndex < 0) currentIndex = images.length - 1;
+  document.getElementById("imgFull").src = images[currentIndex].src;
+}
+
+// Логика свайпа для телефона
+let touchstartX = 0;
+let touchendX = 0;
+
+const modal = document.getElementById("myModal");
+modal.addEventListener(
+  "touchstart",
+  (e) => (touchstartX = e.changedTouches[0].screenX),
+);
+modal.addEventListener("touchend", (e) => {
+  touchendX = e.changedTouches[0].screenX;
+  handleGesture();
 });
+
+function handleGesture() {
+  if (touchendX < touchstartX - 50) changeImage(1); // Свайп влево -> след. фото
+  if (touchendX > touchstartX + 50) changeImage(-1); // Свайп вправо -> пред. фото
+}
