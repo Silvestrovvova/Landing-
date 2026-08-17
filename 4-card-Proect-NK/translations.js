6723bb6675c304f73f6699f4c4ce996602e65c25
Silvestrovvova <sidni168@gmail.com>
2026-08-17 17:39:15 +0200

    update

---
+++ 4-card-Proect-NK/translations.js
@@ -0,0 +1,35 @@
+window.translations = {
+  cz: {
+    nav_services: "Služby",
+    nav_portfolio: "Portfolio",
+    nav_prices: "Ceník",
+    nav_about: "O mně",
+    nav_contacts: "Jak objednat",
+    hero_title: "ESTETICKÉ WEBY PRO ZNAČKY",
+    hero_subtitle:
+      "Tvořím moderní, logické a uživatelsky přívětivé weby i aplikace pro malé a střední podniky v několika jazycích, přizpůsobené vašimvizuálním preferencím.",
+    btn_start: "Začít svůj projekt",
+  },
+  en: {
+    nav_services: "Services",
+    nav_portfolio: "Portfolio",
+    nav_prices: "Prices",
+    nav_about: "About me",
+    nav_contacts: "How to start",
+    hero_title: "Aesthetic websites for brands",
+    hero_subtitle:
+      "I create modern, intuitive and user‑friendly websites and applications for small and medium‑sized businesses in multiple languages, tailored to your visual preferences",
+    btn_start: "Start your project",
+  },
+  ru: {
+    nav_services: "Услуги",
+    nav_portfolio: "Портфолио",
+    nav_prices: "Цены",
+    nav_about: "Обо мне",
+    nav_contacts: "Как начать",
+    hero_title: "Эстетичные сайты для брендов",
+    hero_subtitle:
+      "Создаю современные, логичные и удобные сайты и приложения для малого и среднего бизнеса на нескольких языках, адаптированные под ваши визуальные предпочтения",
+    btn_start: "Начать свой проект",
+  },
+};
